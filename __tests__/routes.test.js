a690eab887c00362371918d40cd332e222327044
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-21 11:27:43 -0700

    wrote GetById test

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,10 +1,9 @@
 import request from 'supertest';
-
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-import app from '../lib/app.js';
 
-// import Order from '../lib/models/Order.js';
+import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
 
 
 describe('service routes', () => {
@@ -12,11 +11,22 @@ describe('service routes', () => {
     return setup(pool);
   });
 
-  test('creates a new order in our database and send a text message notification', async () => {
+  test.skip('creates a new order in our database and send a text message notification', async () => {
     const res = await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
 
     expect(res.body).toEqual({ id: '1', quantity: 10 });
+  });
+
+  test('gets an order by id via GET', async () => {
+    const order = await Order.insert({ quantity: 2 });
+
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
+
   });
 });
